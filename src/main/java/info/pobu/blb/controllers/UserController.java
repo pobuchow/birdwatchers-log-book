6f8dc5ee93e5305384b56f33b89428e767c4a795
pobuchow <pobu1@hotmail.com>
2019-01-06 22:01:00 +0100

    findByNick UserController method + test

--- src/main/java/info/pobu/blb/controllers/UserController.java
+++ src/main/java/info/pobu/blb/controllers/UserController.java
@@ -1,9 +1,11 @@
 package info.pobu.blb.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,32 +21,42 @@ import info.pobu.blb.entities.exceptions.NotValidEmailException;
 import info.pobu.blb.repositories.IUserRepository;
 
 @Controller
-@RequestMapping(path="/user")
+@RequestMapping(path = "/user")
 public class UserController {
-	
-	@Autowired
-	private IUserRepository userRepository;
-	
-	@GetMapping(path="/add")
-	public @ResponseBody User addNewUser (@RequestParam String nick, @RequestParam String email) throws UserValidationFailedException {
 
-		Nick usersNick = null;
-		try {
-			usersNick = new Nick(nick);
-		} catch (NickIsTooShortException | NickIsTooLongException e) {
-			throw new UserValidationFailedException(e.getMessage());
-		}
-		Email usersEmail = null;
-		try {
-			usersEmail = new Email(email);
-		} catch (NotValidEmailException e) {
-			throw new UserValidationFailedException(e.getMessage());
-		}
-		return userRepository.save(new User (usersNick, usersEmail));
-	}
-	
-	@GetMapping(path="/all")
-	public @ResponseBody List<User> getAllUsers() {
-		return userRepository.findAll();
-	}
+    @Autowired
+    private IUserRepository userRepository;
+
+    @GetMapping(path = "/add")
+    public @ResponseBody User addNewUser(@RequestParam String nick, @RequestParam String email)
+            throws UserValidationFailedException {
+
+        Nick usersNick = null;
+        try {
+            usersNick = new Nick(nick);
+        } catch (NickIsTooShortException | NickIsTooLongException e) {
+            throw new UserValidationFailedException(e.getMessage());
+        }
+        Email usersEmail = null;
+        try {
+            usersEmail = new Email(email);
+        } catch (NotValidEmailException e) {
+            throw new UserValidationFailedException(e.getMessage());
+        }
+        return userRepository.save(new User(usersNick, usersEmail));
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping(path = "/get")
+    public @ResponseBody User findByNickLiteral(@RequestParam String nick) {
+        final List<User> result = userRepository.findAll().stream().filter(u -> nick.equals(u.getNick().getLiteral()))
+                .collect(Collectors.toList());
+        Assert.isTrue(result.size() < 2, "result list should contain one or no users");
+
+        return result.isEmpty() ? null : result.get(0);
+    }
 }
--- src/test/java/info/pobu/blb/controllers/UserControllerTest.java
+++ src/test/java/info/pobu/blb/controllers/UserControllerTest.java
@@ -1,5 +1,8 @@
 package info.pobu.blb.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,56 +23,122 @@ import info.pobu.blb.repositories.IUserRepository;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = UserController.class)
 public class UserControllerTest {
-	
-	@MockBean
-	private IUserRepository repository;
-	
-	@Mock
-	private Email emailMock;
-	
-	@Mock
-	private Nick nickMock;
-	
-	@Mock
-	private User userMock;
-	
-	@Autowired
-	private UserController controller;
-	
-	private static final String NICK_LITERAL = "Tester";
-	private static final String  EMAIL_LITERAL = "xyz@qwe.abc";
-	
-	@Before
-	public void mock() {
-		Mockito.when(emailMock.getLiteral()).thenReturn(EMAIL_LITERAL);
-		Mockito.when(nickMock.getLiteral()).thenReturn(NICK_LITERAL);
-		Mockito.when(repository.save(Mockito.any(User.class))).thenReturn(userMock);
-		Mockito.when(userMock.getEmail()).thenReturn(emailMock);
-		Mockito.when(userMock.getNick()).thenReturn(nickMock);
-	}
 
-	@Test
+    @MockBean
+    private IUserRepository repository;
+
+    @Mock
+    private Email emailMock;
+
+    @Mock
+    private Nick nickMock;
+
+    @Mock
+    private User userMock;
+
+    @Autowired
+    private UserController controller;
+
+    private static final String NICK_LITERAL = "Tester";
+    private static final String EMAIL_LITERAL = "xyz@qwe.abc";
+
+    public static final Nick USER_A_NICK;
+    public static final Nick USER_B_NICK;
+    public static final Nick USER_C_NICK;
+    static {
+        try {
+            USER_A_NICK = new Nick("a_test");
+            USER_B_NICK = new Nick("b_test");
+            USER_C_NICK = new Nick("c_test");
+        } catch (final Exception ex) {
+            throw new RuntimeException("Failed to create Nick instance in static block.", ex);
+        }
+    }
+
+    public static final Email USER_A_EMAIL;
+    public static final Email USER_B_EMAIL;
+    public static final Email USER_C_EMAIL;
+    static {
+        try {
+            USER_A_EMAIL = new Email("a@xyz.abc");
+            USER_B_EMAIL = new Email("b@xyz.abc");
+            USER_C_EMAIL = new Email("c@xyz.abc");
+        } catch (final Exception ex) {
+            throw new RuntimeException("Failed to create Email instance in static block.", ex);
+        }
+    }
+
+    static final List<User> USERS = new ArrayList<User>() {
+        /** serialization */
+        private static final long serialVersionUID = -4250775630610091530L;
+        {
+            add(new User(USER_A_NICK, USER_A_EMAIL));
+            add(new User(USER_B_NICK, USER_B_EMAIL));
+            add(new User(USER_C_NICK, USER_C_EMAIL));
+        }
+    };
+
+    @Before
+    public void mock() {
+        Mockito.when(emailMock.getLiteral()).thenReturn(EMAIL_LITERAL);
+        Mockito.when(nickMock.getLiteral()).thenReturn(NICK_LITERAL);
+        Mockito.when(repository.save(Mockito.any(User.class))).thenReturn(userMock);
+        Mockito.when(repository.findAll()).thenReturn(USERS);
+        Mockito.when(userMock.getEmail()).thenReturn(emailMock);
+        Mockito.when(userMock.getNick()).thenReturn(nickMock);
+    }
+
+    @Test
     public void contexLoads() throws Exception {
         Assert.assertNotNull(controller);
     }
-	
-	@Test
-	public void shouldAddNewUser() throws UserValidationFailedException {
-		//when
-		User user = controller.addNewUser(NICK_LITERAL, EMAIL_LITERAL);
-		//then
-		Assert.assertNotNull("Created user should be not null", user);
-		Assert.assertEquals("Users nick: " + NICK_LITERAL, NICK_LITERAL, user.getNick().getLiteral());
-		Assert.assertEquals("Users email: " + EMAIL_LITERAL, EMAIL_LITERAL, user.getEmail().getLiteral());
-	}
-	
-	@Test(expected = UserValidationFailedException.class)
-	public void shouldThrowExceptionWhenNickIsNull() throws UserValidationFailedException {
-		controller.addNewUser(null, EMAIL_LITERAL);
-	}
-	
-	@Test(expected = UserValidationFailedException.class)
-	public void shouldThrowExceptionWhenMailIsNull() throws UserValidationFailedException {
-		controller.addNewUser(NICK_LITERAL, null);
-	}
+
+    @Test
+    public void shouldAddNewUser() throws UserValidationFailedException {
+        // when
+        User user = controller.addNewUser(NICK_LITERAL, EMAIL_LITERAL);
+        // then
+        Assert.assertNotNull("Created user should be not null", user);
+        Assert.assertEquals("Users nick: " + NICK_LITERAL, NICK_LITERAL, user.getNick().getLiteral());
+        Assert.assertEquals("Users email: " + EMAIL_LITERAL, EMAIL_LITERAL, user.getEmail().getLiteral());
+    }
+
+    @Test(expected = UserValidationFailedException.class)
+    public void shouldThrowExceptionWhenNickIsNull() throws UserValidationFailedException {
+        controller.addNewUser(null, EMAIL_LITERAL);
+    }
+
+    @Test(expected = UserValidationFailedException.class)
+    public void shouldThrowExceptionWhenMailIsNull() throws UserValidationFailedException {
+        controller.addNewUser(NICK_LITERAL, null);
+    }
+
+    @Test
+    public void shouldGetAllUsers() {
+        List<User> users = new ArrayList<>();
+        Assert.assertNotNull(users);
+        Assert.assertTrue(users.isEmpty());
+        users = controller.getAllUsers();
+        Assert.assertFalse(users.isEmpty());
+        Assert.assertEquals(USERS.size(), users.size());
+    }
+
+    @Test
+    public void shouldFindUserWithNick_a_test() {
+        String a_nick = USER_A_NICK.getLiteral();
+
+        User user = controller.findByNickLiteral(a_nick);
+
+        Assert.assertNotNull(user);
+        Assert.assertEquals(a_nick, user.getNick().getLiteral());
+    }
+
+    @Test
+    public void shouldNotFingUserWithNick_d_test() {
+        String d_nick = "d_test";
+
+        User user = controller.findByNickLiteral(d_nick);
+
+        Assert.assertNull(user);
+    }
 }
