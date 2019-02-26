ff7efbcf25441ed01d0fc6c83132fe66e0b84fd8
pobuchow <pobu1@hotmail.com>
2019-02-26 23:27:03 +0100

    app component with basic styles

--- blb-client/src/app/app.component.ts
+++ blb-client/src/app/app.component.ts
@@ -1,31 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observation } from './observation';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div style="text-align:center">
-      <h1>
-        {{greeting}}
-      </h1>
-      </div>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent implements OnInit{
 
-        greeting : string;
+        title = "Birdwatchers Log Book";
 
-        constructor(private http: HttpClient){ 	}
-
-        ngOnInit() {
-
-            console.log('you are in ngOnInit');
-            this.http.get<string>('http://localhost:8080/user/greet', {responseType: 'text'}).subscribe(data => {
-                this.greeting = data; });
-  	}
+        constructor(){ 	}
 }
-
