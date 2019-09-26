13a383a0f6a657ebc30df32f4144add9dad346d8
pobuchow <pobu1@hotmail.com>
2019-09-26 21:43:30 +0200

    home controller added

---
+++ src/main/java/info/pobu/blb/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package info.pobu.blb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+}
